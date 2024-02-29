d6cbb6b9a4327479ceeef939ce9ec88445ad8137
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-28 22:19:45 -0600

    contact component

---
+++ contact-lists/src/components/ContactList.jsx
@@ -0,0 +1,25 @@
+const Conatcts = () => {
+    return (
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="3">Contact List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+            {
+              // Map over data here
+            }
+          </tbody>
+        </table>
+      </>
+    );
+}
+ 
+export default Conatcts;
