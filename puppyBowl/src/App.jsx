993bac7234d2bb82059cd897299e8fc4076fe5c4
PatelPratik9 <pratspatel17791@gmail.com>
2024-03-14 14:28:18 -0500

    navbar fromm bootstrap

--- puppyBowl/src/App.jsx
+++ puppyBowl/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
-
-import "./App.css";
+// import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AllPlayer from "./components/AllPlayer.jsx";
+import SinglePlayer from "./components/SinglePlayer.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <>
+      <NavBar />
+      <h1> Puppy Bowl using React</h1>
 
-  return <></>;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AllPlayer />} />
+          <Route path="/players/:id" element={<SinglePlayer />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- puppyBowl/src/components/NavBar.jsx
+++ puppyBowl/src/components/NavBar.jsx
@@ -1,12 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const NavBar = () => {
-    return ( 
-        <>
-            
-        </>
-     );
-}
- 
+  return (
+    <>
+      <nav
+        class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Puppy-Bowl
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  All Players
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Single Player
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Player Form
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
 export default NavBar;
-<>
-    
-</>
